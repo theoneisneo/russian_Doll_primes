3055ddd871ed4b997145bf88ea9df05cddab8cd9
TheOneisNEO <theoneisneo@gmail.com>
2017-11-21 21:25:35 +0800

    add russia_doll_prime.py

---
+++ russia_doll_prime.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/env python3
+
+import time
+t1 = time.time()
+
+def isPrime(n):
+    if n < 2:
+        return False
+    for x in range(2, int(n**0.5) + 1):
+        if n % x == 0:
+            return False
+    return True
+
+rdp = [2, 3, 5, 7]
+add = [1, 3, 5, 7, 9]
+
+for p in rdp:
+    for a in add:
+        s = 10 * p + a
+        if isPrime(s):
+            rdp.append(s)
+
+t2 = time.time()
+
+print(rdp)
+print("cost time : " + str(t2 - t1))
