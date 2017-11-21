dd113bf5c6ebce65418c53ddf811c7609bdf37e2
theoneisneo <theoneisneo@gmail.com>
2017-11-21 21:29:24 +0800

    add missing spaces

--- russia_doll_prime.py
+++ russia_doll_prime.py
@@ -7,7 +7,7 @@ t1 = time.time()
 def isPrime(n):
     if n < 2:
         return False
-    for x in range(2, int(n**0.5) + 1):
+    for x in range(2, int(n ** 0.5) + 1):
         if n % x == 0:
             return False
     return True
